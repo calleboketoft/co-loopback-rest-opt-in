6c7067b44412a3be855cf8842d32974b23706e73
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-09-07 10:51:17 +0200

    support related model methods

--- lbRestOptIn.js
+++ lbRestOptIn.js
@@ -1,5 +1,9 @@
-// Use this module to choose which REST endpoints to enable rather which to disable
-// require('co-loopback-rest-opt-in').enable(MyModel, ['create', 'findById'])
+// Manage which of the automatically generated REST endpoints to expose from Loopback application
+
+/**
+ * Main methods
+ * require('co-loopback-rest-opt-in').enable(MyModel, ['create', 'findById'])
+ */
 
 var restMethods = [
   { name: 'create', isStatic: true },             // POST /[model]
@@ -16,9 +20,38 @@ var restMethods = [
 ]
 
 module.exports.enable = function (modelObj, enable) {
+  enable = enable || []
   restMethods.forEach(function (method) {
     if (enable.indexOf(method.name) === -1) {
       modelObj.disableRemoteMethod(method.name, method.isStatic)
+    }
+  })
+}
+
+/**
+ * Relation methods
+ * require('co-loopback-rest-opt-in').enableRelated(Inbox, 'InboxMessage', ['create', 'delete'])
+ * The relationModelName is found in model.json relations specification
+ */
+
+var restRelationMethods = [
+  { name: 'prototype.__create__', isStatic: true },
+  { name: 'prototype.__destroyById__', isStatic: true },
+  { name: 'prototype.__updateById__', isStatic: true },
+  { name: 'prototype.__get__', isStatic: true },
+  { name: 'prototype.__findById__', isStatic: true },
+  { name: 'prototype.__delete__', isStatic: true },
+  { name: 'prototype.__count__', isStatic: true },
+]
+
+module.exports.enableRelated = function (modelObj, relationModelName, enable) {
+  enable = enable || []
+  restRelationMethods.forEach(function (method) {
+    var enableCurrent = enable.some(function(enableMethod) {
+      return method.name.indexOf(enableMethod) !== -1
+    })
+    if (!enableCurrent) {
+      modelObj.disableRemoteMethod(method.name + relationModelName, method.isStatic)
     }
   })
 }
